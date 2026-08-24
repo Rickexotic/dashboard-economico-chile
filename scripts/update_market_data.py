3268e09a3b20e87294892d84d946e2c016604e3d
Rickexotic <128307082+Rickexotic@users.noreply.github.com>
2026-08-24 18:13:39 -0400

    Update update_market_data.py

--- scripts/update_market_data.py
+++ scripts/update_market_data.py
@@ -1,1 +1,24 @@
+import json
+from datetime import datetime
 
+output = {
+    "meta": {
+        "lastUpdate": datetime.now().strftime(
+            "%Y-%m-%d %H:%M UTC"
+        )
+    }
+}
+
+with open(
+    "data/stock-history.json",
+    "w",
+    encoding="utf-8"
+) as f:
+    json.dump(
+        output,
+        f,
+        indent=2,
+        ensure_ascii=False
+    )
+
+print("stock-history.json updated")
